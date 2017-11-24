d8a9c77b8a1fce1d4e9b59c0ea8e69d3479dddb2
Erik Larsson <erik.mattias.larsson@gmail.com>
2017-11-24 16:15:33 +0100

    fibbo

---
+++ main.js
@@ -0,0 +1,49 @@
+//Un-optimized
+function fibonacci(idx) {
+  if (idx === 0)
+    return 0
+  if (idx === 1)
+    return 1
+  return fibonacci(idx - 1) + fibonacci(idx - 2)
+}
+
+//Optimized by cache
+const superFib = (function() {
+    let cache = {}
+
+    function fibCore(idx) {
+      if (idx === 0)
+        return 0
+      if (idx === 1)
+        return 1
+      return superFib(idx - 1) + superFib(idx - 2)
+    }
+    
+    return idx => {
+      if (typeof cache[idx] === "undefined") {
+        return cache[idx] = fibCore(idx)
+      } else {
+        return cache[idx]
+      }
+    }
+  })()
+
+function test() {
+  const resultList = [
+    0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    21,
+    34
+  ]
+
+  resultList.forEach((desired, idx) => {
+    const result = fibonacci(idx)
+    console.log("idx: " + idx + " yields " + result + ", should yield " + desired + " " + (desired === result))
+  })
+}
